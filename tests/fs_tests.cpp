dae5b85ffc87a3c999952d5a8b1fa4e409a51a67
Alex-Tsvetanov <alex@tsalex.tk>
2018-09-03 17:53:57 +0300

    first commit

--- tests/fs_tests.cpp
+++ tests/fs_tests.cpp
@@ -6,7 +6,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
-TEST_SUITE("FileSystem") {
+TEST_SUITE("FS::FileSystem::") {
 	TEST_SUITE("create_child") {
 		TEST_CASE("should add new file::File") {
 			FS::FileSystem fs;
@@ -34,6 +34,27 @@ TEST_SUITE("FileSystem") {
 			fs ()->create_child (test_folder);
 
 			CHECK(fs ()->get_child_by_name("test_folder") == test_folder);
+		}
+	}
+	TEST_SUITE("rm") {
+		TEST_CASE("should remove an existing file::File") {
+			FS::FileSystem fs;
+
+			REQUIRE(fs () != nullptr);
+			REQUIRE(fs ()->get_size ().to_bytes () == 0);
+
+			file::File* test_file = new file::TxtFile ("test_file_1.txt");
+			fs ()->create_child (test_file);
+
+			CHECK(fs ()->get_child_by_name("test_file_1.txt") == test_file);
+			
+			const std::initializer_list<unsigned char> content = {'A','l','e','x',' ','i', 's',' ','p','r','o','g','r','a','m','m','e','r','.'};
+			test_file->write (new file::TxtContent (content));
+
+			CHECK(fs ()->get_size ().to_bytes () == content.size ());
+
+			fs ()->rm (fs ()->get_child_by_name ("test_file_1.txt"));
+			CHECK(fs ()->get_size ().to_bytes () == 0);
 		}
 	}
 }
